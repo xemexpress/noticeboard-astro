6534a1bd5d2d4a21c874b6c1810b4e4ec19accfc
Jason Luo <22487340+xemexpress@users.noreply.github.com>
2024-07-21 11:15:02 +0800

    Cleaning up

--- .netlify/build/chunks/first_CkPH1oUJ.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./first_zqfAg_Uw.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
