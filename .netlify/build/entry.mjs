7d5879ee222d78b0e81415ecfb4525084b257075
Jason Luo <22487340+xemexpress@users.noreply.github.com>
2024-07-21 11:15:39 +0800

    Minor build

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,31 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_DnTbc0hR.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "927a1a8b-2a5b-4790-9eee-c30f5d5a5c04"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .netlify/v1/functions/ssr/src/content/notices/first.md
@@ -0,0 +1,4 @@
+---
+image: ./images/noticeboard.png
+updatedAt: "2024-07-21T09:55:00+08:00"
+---
